9e017238399951f70926c168faa12e27fa6e86cc
DESKTOP-IBGJ554\gqy <1142014196@qq.com>
2018-04-10 14:47:54 +0800

    project-1重构

--- my-project/project-1/data-from/work/backup20170622/test/template.js
+++
@@ -1,9 +0,0 @@
-function getData(urls, tabl) {
-  var events = [];
-  $.ajax({
-    type: "get",
-    url: urls + "events/class/top",
-    dataType: 'json',
-    success: function (eve) {}
-  });
-}
